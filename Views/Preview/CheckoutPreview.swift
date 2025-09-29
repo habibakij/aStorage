44be473665283983918f2a6fda6f7c80adb9af1c
akij.mia <akij.mia@technonext.com>
2025-09-29 18:06:01 +0600

    cart functionality

---
+++ Views/Preview/CheckoutPreview.swift
@@ -0,0 +1,14 @@
+//
+//  SuccessView_Previews.swift
+//  aStorage
+//
+//  Created by AKIJ KHAN on 2025-08-31.
+//
+
+import SwiftUI
+
+struct SuccessView_Previews: PreviewProvider {
+    static var previews: some View {
+        SuccessView().environmentObject(NavigationManager())
+    }
+}
